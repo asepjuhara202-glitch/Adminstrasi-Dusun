adc7b642b81e54684c1b471ae63848ce4c1f6754
asepjuhara202-glitch <asepjuhara202@gmail.com>
2026-06-16 00:05:29 +0700

    Update mockData.ts

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -1,10 +1,13 @@
 import { Warga, RW, Iuran, TransaksiIuran, Pengajuan, Laporan, JadwalRonda, KegiatanRutin, MutasiLog, PenerimaBantuan, GalleryPhoto } from '../types';
 
 export const INITIAL_RWS: RW[] = [
-  { id: 'RW 01', namaKetua: 'Asep Juhara', wilayah: 'Dusun Timur (RT 01 & 02)', kontak: '0812-3456-7890' },
-  { id: 'RW 02', namaKetua: 'Agus Setiawan', wilayah: 'Dusun Selatan (RT 03 & 04)', kontak: '0821-4567-9012' },
-  { id: 'RW 03', namaKetua: 'Dedi Kurniawan', wilayah: 'Dusun Barat (RT 05 & 06)', kontak: '0838-5678-2345' },
-  { id: 'RW 04', namaKetua: 'Eko Prasetyo', wilayah: 'Dusun Utara (RT 07 & 08)', kontak: '0857-6789-3456' }
+  { id: 'RW 07', namaKetua: 'Miftah', wilayah: 'Dusun III', kontak: '0812-3456-7890' },
+  { id: 'RW 08', namaKetua: 'Darmatin', wilayah: 'Dusun III', kontak: '0821-4567-9012' },
+  { id: 'RW 09', namaKetua: 'Sulam', wilayah: 'Dusun III', kontak: '0838-5678-2345' },
+  { id: 'RW 13', namaKetua: 'Ajang', wilayah: 'Dusun III', kontak: '0857-6789-3456' }
+  { id: 'RW 15', namaKetua: 'Fuad', wilayah: 'Dusun III', kontak: '0857-6789-3456' }
+  { id: 'RW 16', namaKetua: 'Endang', wilayah: 'Dusun III', kontak: '0857-6789-3456' }
+  { id: 'RW 17', namaKetua: 'Jajang', wilayah: 'Dusun III', kontak: '0857-6789-3456' }
 ];
 
 export const INITIAL_WARGA: Warga[] = [
